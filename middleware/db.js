b10d344dfe37a5213a67e18f626c23175ee0552f
ChanuthiJayasingha <chanuthi.avindi@gmail.com>
2026-04-25 20:35:08 +0530

    Updated

--- middleware/db.js
+++ middleware/db.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    // Adding { family: 4 } fixes the Node 22 "ENOTFOUND" error
     await mongoose.connect(process.env.MONGO_URI, { family: 4 });
     console.log('✅ MongoDB Connected Successfully');
   } catch (err) {
